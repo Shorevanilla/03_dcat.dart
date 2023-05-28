c9a0ca111070015ad88aff50a1e772ea73a2a35b
Shorevanilla erier <2455745979@qq.com>
2023-05-28 14:30:49 +0000

    modified:   dcat/bin/dcat.dart 	dcat/bin/aaa.txt

--- dcat/bin/dcat.dart
+++ dcat/bin/dcat.dart
@@ -1,17 +1,48 @@
-import 'package:dcat/dcat.dart' as dcat;
-import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'dart:io';
 
-void main() async {
-  final httpPackageUrl = Uri.https('dart.dev', '/f/packages/http.json');
-  final httpPackageInfo = await http.read(httpPackageUrl);
-  print(httpPackageInfo);
+import 'package:args/args.dart';
 
+const lineNumber = 'line-number';
 
-  final httpPackageResponse = await http.get(httpPackageUrl);
-  if (httpPackageResponse.statusCode != 200) {
-    print('Failed to retrieve the http package!');
-    return;
+void main(List<String> arguments) {
+  exitCode = 0; // presume success
+  final parser = ArgParser()..addFlag(lineNumber, negatable: false, abbr: 'n');
+
+  ArgResults argResults = parser.parse(arguments);
+  final paths = argResults.rest;
+
+  dcat(paths, showLineNumbers: argResults[lineNumber] as bool);
+}
+
+Future<void> dcat(List<String> paths, {bool showLineNumbers = false}) async {
+  if (paths.isEmpty) {
+    // No files provided as arguments. Read from stdin and print each line.
+    await stdin.pipe(stdout);
+  } else {
+    for (final path in paths) {
+      var lineNumber = 1;
+      final lines = utf8.decoder
+          .bind(File(path).openRead())
+          .transform(const LineSplitter());
+      try {
+        await for (final line in lines) {
+          if (showLineNumbers) {
+            stdout.write('${lineNumber++} ');
+          }
+          stdout.writeln(line);
+        }
+      } catch (_) {
+        await _handleError(path);
+      }
+    }
   }
-  print(httpPackageResponse.body);
-  print(httpPackageResponse.statusCode);
+}
+
+Future<void> _handleError(String path) async {
+  if (await FileSystemEntity.isDirectory(path)) {
+    stderr.writeln('error: $path is a directory');
+  } else {
+    exitCode = 2;
+  }
 }
