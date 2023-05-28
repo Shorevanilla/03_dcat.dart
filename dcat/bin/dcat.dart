475167ed72c53da9b17ce930af5b8e9c3c23f97c
Shorevanilla erier <2455745979@qq.com>
2023-05-28 14:30:26 +0000

    new file:   dcat/.gitignore 	new file:   dcat/CHANGELOG.md 	new file:   dcat/README.md 	new file:   dcat/analysis_options.yaml 	new file:   dcat/bin/dcat.dart 	new file:   dcat/lib/dcat.dart 	new file:   dcat/pubspec.yaml 	new file:   dcat/test/dcat_test.dart 	modified:   dcat/bin/dcat.dart 	dcat/bin/aaa.txt

---
+++ dcat/bin/dcat.dart
@@ -0,0 +1,17 @@
+import 'package:dcat/dcat.dart' as dcat;
+import 'package:http/http.dart' as http;
+
+void main() async {
+  final httpPackageUrl = Uri.https('dart.dev', '/f/packages/http.json');
+  final httpPackageInfo = await http.read(httpPackageUrl);
+  print(httpPackageInfo);
+
+
+  final httpPackageResponse = await http.get(httpPackageUrl);
+  if (httpPackageResponse.statusCode != 200) {
+    print('Failed to retrieve the http package!');
+    return;
+  }
+  print(httpPackageResponse.body);
+  print(httpPackageResponse.statusCode);
+}
